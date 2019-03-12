43fe291058ee7c8440ef33f0a00706029785541e
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-12 21:26:03 +0000

    Refactored Core Module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { AuthService } from '../providers/auth.service';
 import { DataService } from '../providers/data.service';
 import { AngularFireAuthModule } from '@angular/fire/auth';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthHeaderInterceptor } from '../providers/token.interceptor';
 import { TokenStorage } from '../providers/token.storage';
 import { AuthGuard } from '../guards/auth.guard';
 import { NonAuthGuard } from '../guards/non-auth.guard';
 
 @NgModule({
-  imports: [CommonModule, AngularFireAuthModule, HttpClientModule],
+  imports: [CommonModule, AngularFireAuthModule],
   providers: [
     AuthService,
     DataService,
