fb6acde24eee5a83d5da0ef4a5e4e7086aca121f
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-12 21:26:21 +0000

    Refactored App Module and Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,18 @@ import { AppComponent } from './app.component';
 // Import routing module
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpModule } from '@ngx-progressbar/http';
+import { NgProgressRouterModule } from '@ngx-progressbar/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
+    NgProgressModule,
+    NgProgressHttpModule,
+    NgProgressRouterModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     CoreModule
