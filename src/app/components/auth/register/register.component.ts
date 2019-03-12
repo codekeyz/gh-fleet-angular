ee14151015ad6426399aa51ef059ff244add9bfd
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-12 21:25:20 +0000

    Fixed Register COmponent

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../providers/auth.service';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl
+} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,11 +14,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-  username = '';
-  password = '';
-  email = '';
+  registerForm: FormGroup;
 
-  constructor() {}
+  constructor(
+    private authSvc: AuthService,
+    private fb: FormBuilder,
+    private router: Router
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.createForm();
+  }
+
+  createForm() {
+    this.registerForm = this.fb.group(
+      {
+        username: [
+          '',
+          Validators.compose([Validators.required, Validators.minLength(6)])
+        ],
+        email: [
+          '',
+          Validators.compose([Validators.required, Validators.email])
+        ],
+        password: ['', Validators.required],
+        passwordagain: ['']
+      },
+      {
+        validator: this.checkPasswords
+      }
+    );
+  }
+
+  checkPasswords(group: FormGroup) {
+    // here we have the 'passwords' group
+    const pass = group.controls.password.value;
+    const confirmPass = group.controls.passwordagain.value;
+
+    return pass === confirmPass ? null : { notSame: true };
+  }
+
+  register() {
+    const email = this.registerForm.value.email;
+    const password = this.registerForm.value.password;
+    const username = this.registerForm.value.username;
+    this.authSvc
+      .register(username, email, password)
+      .toPromise()
+      .then(() => {
+        this.router.navigate(['/auth/signin']);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 }
