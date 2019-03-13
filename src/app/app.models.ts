5a48f5d252004e0d4bcd202d1ac3392a5d84738c
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-13 08:20:57 +0000

    Added Data Models

---
+++ src/app/app.models.ts
@@ -0,0 +1,20 @@
+export interface Vehicle {
+  id: string;
+  name: string;
+  color: string;
+  archived: boolean;
+  fuel_volume_units: string;
+  vehicle_type_name: string;
+  owner: {};
+  images: Image[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface Image {
+  _id: string;
+  name: string;
+  link: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
