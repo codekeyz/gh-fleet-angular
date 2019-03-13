f4f30d11151c6fb028d5956c742b454118c30e8b
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-13 14:13:31 +0000

    Refactord Core Module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,25 +3,32 @@ import { CommonModule } from '@angular/common';
 import { AuthService } from '../providers/auth.service';
 import { DataService } from '../providers/data.service';
 import { AngularFireAuthModule } from '@angular/fire/auth';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthHeaderInterceptor } from '../providers/token.interceptor';
 import { TokenStorage } from '../providers/token.storage';
 import { AuthGuard } from '../guards/auth.guard';
 import { NonAuthGuard } from '../guards/non-auth.guard';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { JwtInterceptor } from '@auth0/angular-jwt';
+import { RefreshTokenInterceptor } from '../providers/refresh-token-interceptor';
 
 @NgModule({
   imports: [CommonModule, AngularFireAuthModule],
   providers: [
     AuthService,
+    JwtInterceptor,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useExisting: JwtInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: RefreshTokenInterceptor,
+      multi: true
+    },
     DataService,
     TokenStorage,
     AuthGuard,
-    NonAuthGuard,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthHeaderInterceptor,
-      multi: true
-    }
+    NonAuthGuard
   ],
   declarations: []
 })
