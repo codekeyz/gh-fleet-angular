5e63352bb42548decfcdef1d9555a75b632bc4c6
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-13 17:04:57 +0000

    Attempt to fix deploy error

--- src/app/components/mainapp/vehicles/vehicles-view/vehicles-view.component.ts
+++ src/app/components/mainapp/vehicles/vehicles-view/vehicles-view.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, interval } from 'rxjs';
+import { DataList, Vehicle } from '../../../../app.models';
+import { startWith, switchMap, distinctUntilChanged } from 'rxjs/operators';
+import { DataService } from '../../../../providers/data.service';
 
 @Component({
   selector: 'app-vehicles-view',
@@ -6,10 +10,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vehicles-view.component.scss']
 })
 export class VehiclesViewComponent implements OnInit {
+  vehicles$: Observable<DataList<Vehicle>>;
 
-  constructor() { }
+  constructor(private dataSvc: DataService) {}
 
   ngOnInit() {
+    this.vehicles$ = interval(5000).pipe(
+      startWith(0),
+      switchMap(() => this.dataSvc.getMyVehicles()),
+      distinctUntilChanged((x, y) => {
+        return JSON.stringify(x.data) === JSON.stringify(y.data);
+      })
+    );
   }
-
 }
--- src/app/components/mainapp/vehicles/vehicles.component.ts
+++ src/app/components/mainapp/vehicles/vehicles.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from '../../../providers/data.service';
-import { Observable, interval } from 'rxjs';
-import { DataList, Vehicle } from '../../../app.models';
-import { startWith, switchMap, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-vehicles',
@@ -10,17 +6,7 @@ import { startWith, switchMap, distinctUntilChanged } from 'rxjs/operators';
   styleUrls: ['./vehicles.component.scss']
 })
 export class VehiclesComponent implements OnInit {
-  vehicles$: Observable<DataList<Vehicle>>;
+  constructor() {}
 
-  constructor(private dataSvc: DataService) {}
-
-  ngOnInit() {
-    this.vehicles$ = interval(5000).pipe(
-      startWith(0),
-      switchMap(() => this.dataSvc.getMyVehicles()),
-      distinctUntilChanged((x, y) => {
-        return JSON.stringify(x.data) === JSON.stringify(y.data);
-      })
-    );
-  }
+  ngOnInit() {}
 }
