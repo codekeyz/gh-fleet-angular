e3f677200183b00b7700fff603193557b063ee5d
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-13 08:21:24 +0000

    Fixing up Dashboard Component

--- src/app/components/mainapp/dashboard/dashboard.component.ts
+++ src/app/components/mainapp/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { Vehicle } from '../../../app.models';
+import { DataService } from '../../../providers/data.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,10 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  vehicles$: Observable<Vehicle[]>;
 
-  constructor() { }
+  constructor(private dataSvc: DataService) {}
 
   ngOnInit() {
+    this.dataSvc.getMyVehicles<Vehicle[]>().subscribe(res => {
+      console.log(res);
+    });
   }
-
 }
