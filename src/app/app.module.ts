5cb4e793ff8c46fcb37664701cfe16ec494cffd8
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-13 14:13:58 +0000

    Refactored Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,16 +11,37 @@ import { CoreModule } from './core/core.module';
 import { NgProgressModule } from '@ngx-progressbar/core';
 import { NgProgressRouterModule } from '@ngx-progressbar/router';
 import { HttpClientModule } from '@angular/common/http';
+import { JwtModule, JWT_OPTIONS } from '@auth0/angular-jwt';
+import { TokenStorage } from './providers/token.storage';
+
+function jwtOptionsFactory(tokenStorage: TokenStorage) {
+  return {
+    tokenGetter: () => {
+      return tokenStorage.getToken();
+    },
+    headerName: 'Account-Token',
+    authScheme: '',
+    blacklistedRoutes: environment.blacklistedRoutes,
+    whitelistedDomains: environment.whitelistedDomains
+  };
+}
 
 @NgModule({
   imports: [
     BrowserModule,
+    CoreModule,
     HttpClientModule,
     NgProgressModule,
     NgProgressRouterModule,
+    JwtModule.forRoot({
+      jwtOptionsProvider: {
+        provide: JWT_OPTIONS,
+        useFactory: jwtOptionsFactory,
+        deps: [TokenStorage]
+      }
+    }),
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    CoreModule
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   declarations: [AppComponent],
   providers: [
