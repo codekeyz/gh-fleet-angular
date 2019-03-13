940981d3c93d642e5a955bfc77a75d4672bd7599
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-13 14:12:41 +0000

    Fixed up my refresh token interceptor

---
+++ src/app/providers/refresh-token-interceptor.ts
@@ -0,0 +1,46 @@
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError, from } from 'rxjs';
+import { catchError, mergeMap } from 'rxjs/operators';
+import { AuthService } from './auth.service';
+import { JwtInterceptor } from '@auth0/angular-jwt';
+
+@Injectable()
+export class RefreshTokenInterceptor implements HttpInterceptor {
+  constructor(
+    private authorizationService: AuthService,
+    private jwtInterceptor: JwtInterceptor
+  ) {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    if (
+      this.jwtInterceptor.isWhitelistedDomain(req) &&
+      !this.jwtInterceptor.isBlacklistedRoute(req)
+    ) {
+      return next.handle(req).pipe(
+        catchError(err => {
+          const errorResponse = err as HttpErrorResponse;
+          if (errorResponse.status === 401) {
+            return from(this.authorizationService.refresh()).pipe(
+              mergeMap(() => {
+                return this.jwtInterceptor.intercept(req, next);
+              })
+            );
+          }
+          return throwError(err);
+        })
+      );
+    } else {
+      return next.handle(req);
+    }
+  }
+}
