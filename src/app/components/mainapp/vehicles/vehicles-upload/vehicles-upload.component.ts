85fd02a2992c9b85ec5c9725b252839e9cc571fe
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-14 15:45:06 +0000

    Fixed up vehicle add functionality

--- src/app/components/mainapp/vehicles/vehicles-upload/vehicles-upload.component.ts
+++ src/app/components/mainapp/vehicles/vehicles-upload/vehicles-upload.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { NgProgressRef, NgProgress } from '@ngx-progressbar/core';
+import { DataService } from '../../../../providers/data.service';
+import { ToastrService } from 'ngx-toastr';
+import { Data, Vehicle } from '../../../../app.models';
 
 @Component({
   selector: 'app-vehicles-upload',
@@ -8,11 +12,19 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class VehiclesUploadComponent implements OnInit {
   uploadForm: FormGroup;
+  progressRef: NgProgressRef;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private toastr: ToastrService,
+    private fb: FormBuilder,
+    private progress: NgProgress,
+    private dataSvc: DataService
+  ) {}
 
   ngOnInit() {
     this.createForm();
+
+    this.progressRef = this.progress.ref('masterProgress');
   }
 
   createForm() {
@@ -23,23 +35,61 @@ export class VehiclesUploadComponent implements OnInit {
       ],
       type: [
         '',
-        Validators.compose([Validators.required, Validators.minLength(5)])
+        Validators.compose([Validators.required, Validators.minLength(3)])
       ],
       model: [
         '',
-        Validators.compose([Validators.required, Validators.minLength(5)])
+        Validators.compose([Validators.required, Validators.minLength(3)])
       ],
       color: [
         '',
-        Validators.compose([Validators.required, Validators.minLength(5)])
+        Validators.compose([Validators.required, Validators.minLength(3)])
       ],
       license: [
         '',
-        Validators.compose([Validators.required, Validators.minLength(5)])
+        Validators.compose([Validators.required, Validators.minLength(3)])
       ],
       volume: ['Litres', Validators.compose([Validators.required])]
     });
   }
 
-  upload() {}
+  upload() {
+    const name = this.uploadForm.value.name;
+    const license_plate = this.uploadForm.value.license;
+    const fuel_volume_units = (this.uploadForm.value.volume as string)
+      .toLocaleLowerCase()
+      .split(' ')
+      .join('_');
+    const color = this.uploadForm.value.color;
+    const vehicle_type_model = this.uploadForm.value.model;
+    const vehicle_type_name = this.uploadForm.value.type;
+    this.progressRef.start();
+    this.dataSvc
+      .addVehicle<Data<Vehicle>>({
+        name,
+        license_plate,
+        fuel_volume_units,
+        color,
+        vehicle_type_model,
+        vehicle_type_name
+      })
+      .toPromise()
+      .then(res => {
+        this.uploadForm.reset();
+        this.progressRef.complete();
+        this.toastr.success('Your Vehicle has been added successfully', null, {
+          closeButton: true,
+          timeOut: 3000
+        });
+      })
+      .catch(err => {
+        this.uploadForm.reset();
+        this.progressRef.complete();
+        this.toastr.error(
+          'Your Vehicle could not be added. Try again later',
+          'Oops!'
+        );
+        console.log(err);
+      });
+  }
 }
