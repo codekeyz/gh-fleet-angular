cb1e50d9461d86c8b1f2f80c9ff02c0a01153f5f
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-15 18:29:59 +0000

    Refactored DataService

--- src/app/providers/data.service.ts
+++ src/app/providers/data.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpParams,
+  HttpEvent
+} from '@angular/common/http';
 import { environment as env } from '../../environments/environment';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class DataService {
@@ -20,6 +26,24 @@ export class DataService {
     return this.httpSvc.post<T>(
       `${env.apiBaseUrl}/api/v1/users/me/vehicles`,
       data
+    );
+  }
+
+  uploadImageforVehicle(vehicleID: string, file) {
+    const headers = new HttpHeaders();
+    // this is the important step. You need to set content type as null
+    headers.set('Content-Type', null);
+    headers.set('Accept', 'multipart/form-data');
+    const formData: FormData = new FormData();
+    formData.append('image', file);
+    return this.httpSvc.post(
+      `${env.apiBaseUrl}/api/v1/users/me/vehicles/${vehicleID}/images/upload`,
+      formData,
+      {
+        headers,
+        reportProgress: true,
+        observe: 'events'
+      }
     );
   }
 }
