86de78cc45450c3e61630de21c22369d475df085
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-15 18:30:07 +0000

    Refactord Project

--- src/app/components/mainapp/vehicles/vehicles.module.ts
+++ src/app/components/mainapp/vehicles/vehicles.module.ts
@@ -5,21 +5,28 @@ import { VehiclesViewComponent } from './vehicles-view/vehicles-view.component';
 import { VehiclesSingleComponent } from './vehicles-single/vehicles-single.component';
 import { VehiclesUploadComponent } from './vehicles-upload/vehicles-upload.component';
 import { VehiclesComponent } from './vehicles.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { ProgressbarModule } from 'ngx-bootstrap/progressbar';
+import { FileUploadModule } from 'ng2-file-upload';
+import { ImagePreview } from '../../../providers/image-preview.directive';
 
 @NgModule({
   imports: [
     CommonModule,
+    FileUploadModule,
     PaginationModule.forRoot(),
     BsDropdownModule.forRoot(),
+    ProgressbarModule.forRoot(),
     CarouselModule,
+    FormsModule,
     ReactiveFormsModule,
     VehicleRoutingModule
   ],
   declarations: [
+    ImagePreview,
     VehiclesComponent,
     VehiclesViewComponent,
     VehiclesSingleComponent,
